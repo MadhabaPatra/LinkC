bee15756795438689f178402b841a0db6c7cd1d5
pikaz143 <42296778+pikaz143@users.noreply.github.com>
2019-09-12 02:08:44 +0530

    Deploy app

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views','./views');
 app.use(express.static('views'));
+var PORT=process.env.PORT || 9000;
 app.get('/linkc/:id', function (req, res) {
    console.log('working');
    //res.send('your id is : '+req.params.id);
@@ -16,7 +17,7 @@ app.get('/linkc/:id', function (req, res) {
    res.render('userpage',{
       id:req.params.id
    } );
-}).listen(9000 , () => console.log('Example app listening on port 9000!'))
+}).listen(PORT)
 
 
 /*var server = app.listen(8081, function () {
